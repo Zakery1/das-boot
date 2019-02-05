0437aa9f9d3c13723644f0b8cdc21a391a9118a8
Zachary Graham <zakgraham@gmail.com>
2019-02-04 17:19:32 -0700

    use J7 to run app

--- src/main/java/com/boot/App.java
+++ src/main/java/com/boot/App.java
@@ -1,6 +1,6 @@
 package com.boot;
 
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -12,6 +12,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
